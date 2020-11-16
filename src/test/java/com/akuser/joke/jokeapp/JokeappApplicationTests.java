ccbb87be1862c427f2c481f8837e5c4be7379fd1
Anton Kopchak <anton.kopchak@vitech.com.ua>
2020-11-16 21:08:11 +0200

    init commit

---
+++ src/test/java/com/akuser/joke/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akuser.joke.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
